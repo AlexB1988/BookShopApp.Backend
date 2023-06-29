45015ea86411224790d8e0b2c203bd55943ef6e1
AlexB1988 <92520323+AlexB1988@users.noreply.github.com>
2023-06-29 08:52:13 +0300

    Добавлен PriceController

---
+++ BookShopApp.WebApi/Controllers/PriceController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using BookShopApp.Application.CQRS.Books.Commands.Create;
+using BookShopApp.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class PriceController:BaseController
+    {
+        private IMapper _mapper;
+
+        public PriceController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        //[HttpGet("{bbokId}")]
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create([FromBody] CreateBookDto bookDto)
+        {
+            var command=_mapper.Map<CreateBookCommand>(bookDto);
+
+            var priceId=await Mediator.Send(command);
+
+            return Ok(priceId);
+        }
+    }
+}
