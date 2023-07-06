e030ce253590fa089edc8400bef1fb93873214f2
AlexB1988 <bochik_88@mail.ru>
2023-07-06 07:15:31 +0300

    Убрал маппинг UpdateAuthorCommand

--- BookShopApp.Application/CQRS/Authors/Commands/Update/UpdateAuthorCommand.cs
+++ BookShopApp.Application/CQRS/Authors/Commands/Update/UpdateAuthorCommand.cs
@@ -7,22 +7,11 @@ using Newtonsoft.Json;
 
 namespace BookShopApp.Application.CQRS.Authors.Commands.Update
 {
-    public class UpdateAuthorCommand : IRequest<Unit>,IMapWith<Author>
+    public class UpdateAuthorCommand : IRequest<Unit>
     {
         [JsonIgnore]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Biography { get; set; }
-
-        public void Mapping(Profile profile)
-        {
-            profile.CreateMap<UpdateAuthorCommand, Author>()
-                .ForMember(author => author.Id,
-                    opt => opt.MapFrom(authorUpdate => authorUpdate.Id))
-                .ForMember(author => author.Name,
-                    opt => opt.MapFrom(authorUpdate => authorUpdate.Name))
-                .ForMember(author => author.Biography,
-                    opt => opt.MapFrom(authorUpdate => authorUpdate.Biography));
-        }
     }
 }
